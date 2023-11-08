a9697d5540c558faecd275e7a567d36728a7314e
ChristianDj456 <79539982+ChristianDj456@users.noreply.github.com>
2023-11-08 16:06:09 +0000

    Se solucionó el problema con la consulta del promedio por horas, y se agregaron la media y mediana, pero no funcionan

--- dbt-template/models/Querys/PromByHours.sql
+++ dbt-template/models/Querys/PromByHours.sql
@@ -1,11 +1,11 @@
 {{ config(materialized='table') }}
 
-SELECT
+SELECT DISTINCT
   Codigo,
   CodigoSensor,
-  DATE(FechaObservacion) AS Fecha,
+  DATETIME(DATE(FechaObservacion), TIME(EXTRACT(HOUR FROM FechaObservacion),0,0)) AS Fecha,
   EXTRACT(HOUR FROM FechaObservacion) AS Hora,
-  AVG(Viento) AS PromedioValorObservado,
+  AVG(Viento) OVER(PARTITION BY  DATETIME(DATE(FechaObservacion), TIME(EXTRACT(HOUR FROM FechaObservacion),0,0))) AS PromedioVel,
   NombreEstacion,
   Departamento,
   Municipio,
@@ -16,16 +16,7 @@ SELECT
   Region,
 FROM
   {{ ref('Regiones') }}
-GROUP BY
+ORDER BY
   Codigo,
-  CodigoSensor,
-  DATE(FechaObservacion),
-  EXTRACT(HOUR FROM FechaObservacion),
-  NombreEstacion,
-  Departamento,
-  Municipio,
-  ZonaHidrografica,
-  Latitud,
-  Longitud,
-  DescripcionSensor,
-  Region
+  Fecha,
+  Hora
