7d9914fbe4028f3df31da60e96d372810540c147
ChristianDj456 <79539982+ChristianDj456@users.noreply.github.com>
2023-11-09 02:00:05 +0000

    Se agrego la columna Region  a la tabla Direccion del Viento

---
+++ dbt-template/models/Querys/PromHorasDir.sql
@@ -0,0 +1,20 @@
+SELECT DISTINCT
+  Codigo,
+  CodigoSensor,
+  DATETIME(DATE(FechaObservacion), TIME(EXTRACT(HOUR FROM FechaObservacion),0,0)) AS Fecha,
+  EXTRACT(HOUR FROM FechaObservacion) AS Hora,
+  AVG(Viento) OVER(PARTITION BY  DATETIME(DATE(FechaObservacion), TIME(EXTRACT(HOUR FROM FechaObservacion),0,0))) AS PromedioDir,
+  NombreEstacion,
+  Departamento,
+  Municipio,
+  ZonaHidrografica,
+  Latitud,
+  Longitud,
+  DescripcionSensor,
+  Region,
+FROM
+  {{ ref('RegionesDir') }}
+ORDER BY
+  Codigo,
+  Fecha,
+  Hora
