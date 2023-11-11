c366404e74f0b32c21eeb81eb1a4af44bd20154c
ChristianDj456 <79539982+ChristianDj456@users.noreply.github.com>
2023-11-11 03:53:27 +0000

    Se agregó la region Insular

--- dbt-template/models/WindSpeed.sql
+++ dbt-template/models/WindSpeed.sql
@@ -1,3 +1,4 @@
+{{ config(materialized='ephemeral')}}
 SELECT CodigoEstacion, CodigoSensor, FechaObservacion, ValorObservado, NombreEstacion, TRANSLATE(UPPER(Departamento), 'ÁÉÍÓÚÜÑ', 'AEIOUUN') AS Departamento, 
 TRANSLATE(UPPER(Municipio), 'ÁÉÍÓÚÜÑ', 'AEIOUUN') AS Municipio, ZonaHidrografica, Latitud, Longitud, DescripcionSensor, UnidadMedida
 FROM {{ source('WindsForecast', 'WindSpeed_EXT') }}
